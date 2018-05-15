d7cbe6cadaa8ef035c6c2c0cac1fd1b01fddf7b8
Sam Lea <sam.lea@aviatnet.com>
2018-05-15 14:59:59 +1200

    Client side redesign: apply template based on active state of each message + add controls

--- public/js/msgTemplate.js
+++ public/js/msgTemplate.js
@@ -1,18 +1,27 @@
-function applyTemplate(newMsg) {
-  let foo = utf8.encode(newMsg.message);
-  let markup = `
-    <div id="msg${newMsg._id}" class="containerMessage" style="background: #DDDDDD" index="msg${newMsg.index}">
-      <p class="index" title="Message Index" style="display: flex">${newMsg.index}</p>
-      <p class="timestamp" title="Timestamp" style="display: none">${newMsg.timestamp}</p>
-      <span class="spacer" style="display: block;margin-left: 24px"></span>
-      <span id="msg${newMsg._id}upLevel" class="triangle-topright" status="inactive" style="display:none;"></span>
-      <p id="msg${newMsg._id}fold" style="display:flex;" title="Unfold higher level logs" class="containerNoFold"></p>
-      <p class="logLevel" title="Log Level" style="display: flex">${newMsg.level}</p>
-      <p class="levelStep" title="Step" style="display: none">${newMsg.step}</p>
-      <span class="triangle-right" style="display: block"></span>
-      <pre id="msg${newMsg._id}content" class="None">${foo}</pre>
-      <span>&zwnj;</span>
-    </div>`
-  //console.log(markup);
-  return markup;
+function getMarkup(msgData) {
+  let levelChangeDisplay;
+  if (userControls.tabs) {
+    levelChangeDisplay = msgData.levelDisplay.levelChange;
+  } else
+    levelChangeDisplay = getDisplay(userControls.tabs);
+  return `
+  <div id="msg${msgData.index}" class="containerMessage" style="background: #DDDDDD" index="${msgData.index}">
+    <p class="debug" title="developer info" style="display: ${getDisplay(userControls.dev)}">${msgData._id}:${msgData.parents}</p>
+    <p class=${msgData.indexClass} title="Message Index" style="display: ${getDisplay(userControls.index)}">${msgData.index}</p>
+    <p class="timestamp" title="Timestamp" style="display: ${getDisplay(userControls.ts)}">${msgData.timestamp}</p>
+    <span class="spacer" style="display: ${getDisplay(userControls.tabs)};margin-left: ${msgData.levelDisplay.spacerWidth}px"></span>
+    <span id="msg${msgData.index}upLevel" class="triangle-topright" style="display:${levelChangeDisplay};"></span>
+    <p id="msg${msgData.index}fold" style="display: ${getDisplay(userControls.folding)};" title="${msgData.foldDisplay.tooltip}" class="container${msgData.foldDisplay.container}Fold">${msgData.foldDisplay.content}</p>
+    <p class="logLevel" title="Log Level" style="display: ${getDisplay(userControls.levels)}">${msgData.level}</p>
+    <p class="levelStep" title="Step" style="display: ${getDisplay(userControls.steps)}">${msgData.step}</p>
+    <span class="triangle-right" style="display: ${getDisplay(userControls.levels)}"></span>
+    <pre id="msg${msgData.index}content" class="None">${msgData.message}</pre>
+    <span>&zwnj;</span>
+  </div>`;
+}
+
+function getBasicMarkup(msgData) {
+  return `
+  <p id="basic${msgData.index}">${msgData.index} ${msgData.timestamp} ${msgData.message}</p>
+  `;
 }
--- public/js/processRx.js
+++ public/js/processRx.js
@@ -1,5 +1,126 @@
-let allData = [];
+let allMsgs = [];  // All messages as js objects
+let activeMsgIndices = [];  // The current unfolded message indices
+let activeHtml = []; // Currently active (unfolded) message HTML markup
 let clusterize;
+
+function formatTimestamp(rxTimestamp) {
+  let date = new Date(rxTimestamp);
+  let ts = [date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getUTCMilliseconds()];
+  for (let x = 0;  x < ts.length; x++) {
+    if (ts[x] < 10) {
+      ts[x] = "0" + ts[x];
+    }
+  }
+  return ts[0] + ":" + ts [1] + ":" + ts[2] + "." + ts[3];
+}
+
+function formatFolding(numOfChildren) {
+  let display = {container: "", content: "", tooltip: ""};
+  if (numOfChildren > 0) {
+      if (userControls.foldAll == "on") {
+        display.content = "+"
+        display.tooltip = "Unfold higher level logs"
+      } else {
+        display.content = "-"
+        display.tooltip = "Fold higher level logs"
+      }
+    } else {
+      // Message is (currently) not a parent (has no children)
+      display.container = "No";
+    }
+  return display
+}
+
+function getSpacerWidth(level, step) {
+  if (level > 1 && step === 1) {
+    return {
+      levelChange: "block",
+      spacerWidth: (level - 2) * 24
+    };
+  } else {
+    return {
+      levelChange: "none",
+      spacerWidth: (level - 1) * 24
+    };
+  }
+}
+
+function getFoldState(level, parentIndices) {
+  let folded = false;
+  if (level > 1) {
+    // Check global fold all state, this state can be overridden by the fold status of the parents
+    if (userControls.foldAll == "on") {
+      folded = true;
+    }
+    for (let i = 0; i < newMsg.level-1; i++) {
+      // Check content of fold element of parent as soon as a parent is folded break out
+      // Can't use getElementById because parent might not be in current (clusterize) cluster
+      if (allMsgs[parentIndices[i] - 1].foldState) {
+        folded = true;
+        break;
+      }
+    }
+  }
+  return folded
+}
+
+function constructMessage(rxMsg) {
+  return {
+    // Received message params that do not change after being received
+    message: utf8.encode(rxMsg.message),
+    index: rxMsg.index,
+    step: rxMsg.step,
+    level: rxMsg.level,
+    numOfChildren: rxMsg.numOfChildren,
+    timestamp: formatTimestamp(rxMsg.timestamp),
+    // Display parameters that can be modified by user input
+    foldState: getFoldState(),
+    foldDisplay: formatFolding(rxMsg.numOfChildren),
+    indexClass: "index",
+    levelDisplay: getSpacerWidth(rxMsg.level, rxMsg.step),
+    // Debug
+    parentIndices: rxMsg.getParents,
+    _id: rxMsg._id.slice(-4),
+    parents: rxMsg.parents.map(function(item) {
+      return item.slice(-4);
+    }),
+  };
+}
+
+
+class UserControls {
+  constructor() {
+    this.index = true;  // initially checked / on
+    this.ts = false;  // initially unchecked / off         flex, none
+    this.folding = true;                                // flex, none
+    this.tabs = true;                                   // block, none
+    this.levels = true;                                 // flex and block, none
+    this.steps = false;                                 // flex, none
+    this.foldAll = undefined;
+    this.basic = false;  // Whether basic or clusterize logs are displayed
+    this.dev = false;
+  }
+}
+
+let userControls = new UserControls();
+
+function getDisplay(control) {
+  if (control) {
+    return "flex";
+  } else {
+    return "none";
+  }
+}
+
+function updateActive() {
+  // Reapply all global controls to template for every active (unfolded) message
+  // Currently folded messages are updated with the global control state when they are unfolded (made active)
+  for (let i=0; i<activeMsgIndices.length; i++) {
+    activeHtml[i] = getMarkup(allMsgs[activeMsgIndices[i]-1]);
+  }
+  clusterize.update(activeHtml);
+  clusterize.refresh(true);
+}
 
 $(window).ready(function(){
   // Set the height of the scroll area
@@ -13,31 +134,84 @@ $(window).ready(function(){
     contentId: 'contentArea'
   });
 
+  $(document).on("click", ".containerFold", function() {
+    let clickedMsgIndex = parseInt(this.id.slice(3, -4));
+    // Update the parent message
+    console.log("clicked message: " + clickedMsgIndex);
+    console.log("current child fold state is " + allMsgs[clickedMsgIndex-1].foldState);
+    console.log("number of children to process: " + allMsgs[clickedMsgIndex-1].numOfChildren);
+    let activeIndex = activeMsgIndices.indexOf(clickedMsgIndex)
+    if (allMsgs[clickedMsgIndex-1].foldState) {
+      console.log("update clicked element (parent) to unfold, active index: " + activeIndex)
+      setParentUnfolded(clickedMsgIndex-1);
+      activeHtml[activeIndex] = getMarkup(allMsgs[clickedMsgIndex-1])
+
+      console.log("unfold children")
+      let insertActiveIndex = activeIndex + 1;
+      for (let i=clickedMsgIndex; i<=clickedMsgIndex+allMsgs[clickedMsgIndex-1].numOfChildren; i++) {
+        if (activeMsgIndices.indexOf(i+1) === -1) {
+          // Child is not already inserted
+          if (allMsgs[i].foldState  && allMsgs[i].numOfChildren > 0) {
+            // Child is a parent and is folded so add it and skip its children
+            activeHtml.splice(insertActiveIndex, 0, getMarkup(allMsgs[i]))
+            activeMsgIndices.splice(insertActiveIndex, 0, allMsgs[i].index);
+            i += allMsgs[i].numOfChildren;
+          } else {  // check the message
+            activeHtml.splice(insertActiveIndex, 0, getMarkup(allMsgs[i]))
+            activeMsgIndices.splice(insertActiveIndex, 0, allMsgs[i].index);
+          }
+        }
+        insertActiveIndex++;
+      }
+    } else {
+      console.log("update clicked element (parent) to fold, active index: " + activeIndex)
+      setParentFolded(clickedMsgIndex-1);
+      activeHtml[activeIndex] = getMarkup(allMsgs[clickedMsgIndex-1])
+
+      console.log("fold children")
+      for (let i=activeIndex+1; i<=activeIndex+allMsgs[clickedMsgIndex-1].numOfChildren; i++) {
+        if (activeMsgIndices[activeIndex+1] <= activeMsgIndices[activeIndex]+allMsgs[clickedMsgIndex-1].numOfChildren) {
+          activeHtml.splice(activeIndex+1, 1);  // remove index for each subsequent child
+          activeMsgIndices.splice(activeIndex+1, 1);
+        } else {
+          break;
+        }
+      }
+    }
+    clusterize.update(activeHtml);
+    clusterize.refresh(true);
+  });
+
   $(function () {
     let socket = io();
+    // Single (live db update) log message
     socket.on('log message', function(msg){
-      // console.log(msg)
       // Check message is a new message - just log updates to console for now
       if (msg.o.hasOwnProperty("index") && msg.o.message.charAt(0) != "{") {
         $('#main').append(applyTemplate(msg));
         newDomDiv = applyTemplate(msg);
         // console.log(newDomDiv)
-        allData.push(newDomDiv);
-        clusterize.update(allData);
+        // allData.push(newDomDiv);
+        // clusterize.update(allData);
       } else {
         // console.log(msg);
       }
       // window.scrollTo(0, document.body.scrollHeight);
     });
+    // 1+ (already inserted) messages
     socket.on('saved messages', function(docs){
+      // TODO check for duplicate messages
       // console.log(docs.length + " messages received")
       docs.forEach(function (value) {
-        // console.log(value)
-        newDomDiv = applyTemplate(value);
-        allData.push(newDomDiv);
+        let msg = constructMessage(value);
+        allMsgs.push(msg);
+        let msgMarkup = getMarkup(msg);
+        activeHtml.push(msgMarkup);
+        activeMsgIndices.push(msg.index);
       })
-      clusterize.update(allData); // TODO could use .append here until the data is fully loaded?
-      clusterize.refresh(true)  // refresh to update the row heights
+
+      clusterize.update(activeHtml);
+      clusterize.refresh(true);  // refresh to update the row heights
       // refresh seems to fix the following issues:
       // not being able to scroll to bottom/flickering
       // skipping records when scrolling past cluster transitions
@@ -47,3 +221,15 @@ $(window).ready(function(){
     });
   });
 });
+
+function setParentFolded(allMsgsIndex) {
+  allMsgs[allMsgsIndex].foldState = true;
+  allMsgs[allMsgsIndex].foldDisplay.content = "+";
+  allMsgs[allMsgsIndex].foldDisplay.tooltip = "Unfold higher level logs";
+}
+
+function setParentUnfolded(allMsgsIndex) {
+  allMsgs[allMsgsIndex].foldState = false;
+  allMsgs[allMsgsIndex].foldDisplay.content = "-";
+  allMsgs[allMsgsIndex].foldDisplay.tooltip = "Fold higher level logs";
+}
